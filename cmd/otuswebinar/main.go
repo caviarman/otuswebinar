f5038d5b101fdf026f57884318a670e7cc667235
Vladimir Ilin <vladimir_ilin@hotmail.com>
2020-08-03 16:59:23 +0400

    add server

--- cmd/otuswebinar/main.go
+++ cmd/otuswebinar/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"net/http"
 	"os"
 
 	"github.com/sirupsen/logrus"
@@ -13,4 +14,8 @@ func main() {
 	if len(port) == 0 {
 		logrus.Fatal("Port is not set")
 	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
+	http.ListenAndServe(":"+port, nil)
 }
